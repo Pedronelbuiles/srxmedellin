125d1a007f76fb3c89d2af9ed82434d99bbf7055
Pedronelbuiles <pedronel.builes@gmail.com>
2017-06-13 12:26:17 -0500

    'inclusionDeFormularioDeContactoYActualizacionDeRedesSociales'

--- js/app.js
+++ js/app.js
@@ -16,3 +16,14 @@ $offCanvas.find('li').click(function (ev) {
 })
 // var menu = new Foundation.DropdownMenu($('#main-menu'));
 
+// closes the panel on click outside
+$(document).mouseup(function (e) {
+  var container = $('#contact-panel');
+  if (!container.is(e.target) // if the target of the click isn't the container...
+  && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+      container.removeClass('is-active');
+    }
+});
+
+
